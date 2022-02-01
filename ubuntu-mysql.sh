4c553b725bdfbed6f6baeb85a5baa83b090cff0d
trungnguyen <andromeda9096@gmail.com>
2022-02-01 09:53:12 +0700

    Update ubuntu-mysql.sh

--- ubuntu-mysql.sh
+++ ubuntu-mysql.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+sudo apt-get update -y
 
-sudo apt-get update -y && sudo apt install mysql-server -y
-
+sudo apt install mysql-server -y
 sudo service mysql start
 
-export REMOTE_USER=myuser
-export REMOTE_PW=MyPW@123
+export REMOTE_USER=admin
+export REMOTE_PW=Abcd@2468
+sudo mysql -e "CREATE DATABASE mysite;"
 
-mysql -e "CREATE USER '$REMOTE_USER' IDENTIFIED BY '$REMOTE_PW'"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$REMOTE_USER' WITH GRANT OPTION;"
-
-sed -i -E 's/^bind-address[\t ]*= 127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo mysql -e "CREATE USER '$REMOTE_USER' IDENTIFIED BY '$REMOTE_PW'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$REMOTE_USER' WITH GRANT OPTION;"
+sudo sed -i -E 's/^bind-address[\t ]*= 127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
 
 sudo service mysql restart
 
